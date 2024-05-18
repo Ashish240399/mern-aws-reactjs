02a0085fa0704bd57d4a98bb488e08f966beedc6
Ashish Kumar Mohanty <ashishkumarmohanty7797@gmail.com>
2024-05-18 21:22:16 +0530

    latest react commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getData().catch(err => setError(err));
+  }, [])
+
+  async function getData() {
+    const response = await fetch("http://localhost:8900/hello");
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  
+    const data = await response.json();
+  
+    console.log('Response data:', data);
+    setData(data.message);
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <h1>{data}</h1>
+    </>
+  )
+}
+
+export default App
